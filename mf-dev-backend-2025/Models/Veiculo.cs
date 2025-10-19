ec6f43e7433edc175cb6909b52a890ca9e4501fb
Leandro Silva <leandroameliodasilva@gmail.com>
2025-10-18 22:50:13 -0300

    Creat

--- mf-dev-backend-2025/Models/Veiculo.cs
+++ mf-dev-backend-2025/Models/Veiculo.cs
@@ -16,9 +16,11 @@ namespace mf_dev_backend_2025.Models
         public string Placa { get; set; }
 
         [Required(ErrorMessage = "O ano de fabricação é obrigatório.")]
+        [Display(Name = "Ano de Fabricação")]
         public int AnoFabricacao { get; set; }
 
         [Required(ErrorMessage = "O ano do modelo é obrigatório.")]
+        [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
     }
 }
